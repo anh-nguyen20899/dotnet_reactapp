33eb41d066ecce3cbdb4449f9337a4b8c5dcffe7
anhng <anananh1999@gmail.com>
2023-07-20 11:08:51 +1000

    Create Grid Ranking System

--- dotnet_reactapp/ClientApp/src/components/RankItem.js
+++ dotnet_reactapp/ClientApp/src/components/RankItem.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages';
-
+import RankingGrid from './RankingGrid';
 const RankItem = () => {
     const [items, setItems] = useState([]);
     const dataType = 1;
@@ -15,11 +15,15 @@ const RankItem = () => {
     },[])
     return(
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr}/>
             <div className='items-not-ranked'>
             {
+                
                 (items.length > 0) ? items.map((item) =>
-                    
+                <div className='cell-not-ranked'>
                     <img id={`id-${item.id}`} key={item.id} src={MovieImageArr.find(o => o.id === item.imageId)?.image}/>
+                </div>
+                    
                     ): <div>... Loading</div>
             }
             
