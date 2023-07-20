338b39c050205770dde54d79f64f2edb81de2b3d
anhng <anananh1999@gmail.com>
2023-07-20 14:34:24 +1000

    Create Draggable Items

--- dotnet_reactapp/ClientApp/src/components/RankItem.js
+++ dotnet_reactapp/ClientApp/src/components/RankItem.js
@@ -13,15 +13,37 @@ const RankItem = () => {
                 setItems(data);
             })
     },[])
+    function drag(event) {
+        event.dataTransfer.setData("text", event.target.id);
+    }
+    function allowDrop(event) {
+        event.preventDefault();
+    }
+    function drop(event) {
+        event.preventDefault();
+        const targetElm = event.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(event.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+            { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+    }
     return(
         <main>
-            <RankingGrid items={items} imgArr={MovieImageArr}/>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop}/>
             <div className='items-not-ranked'>
             {
                 
                 (items.length > 0) ? items.map((item) =>
-                <div className='cell-not-ranked'>
-                    <img id={`id-${item.id}`} key={item.id} src={MovieImageArr.find(o => o.id === item.imageId)?.image}/>
+                <div className='unranked-cell'>
+                    <img id={`id-${item.id}`} 
+                        key={item.id} 
+                        style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+                        src={MovieImageArr.find(o => o.id === item.imageId)?.image}/>
                 </div>
                     
                     ): <div>... Loading</div>
